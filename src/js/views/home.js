68f639fc32cb29e5d34f2bd6b4874dc410cc672f
Mathis Tressard <mathistressard44@gmail.com>
2021-09-24 15:22:32 +0200

    replace html template strings

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,6 @@
 import "../components/counter.js";
 
-export default () => /*html*/`
+export default () => html`
     <h1>Home</h1>
     <p>Simple click counter</p>
     <click-counter></click-counter>
